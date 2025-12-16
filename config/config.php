d92858d39b67873096efb0dadef8cf0feb0ee955
ramiabdelghafour <itmeabdelghafour@gmail.com>
2025-12-16 14:39:35 +0100

    set project structure

---
+++ config/config.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+$servername = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'coachpro';
+
+try {
+    $conn = new mysqli($servername, $user, $pass, $dbname);
+    
+    if($conn->connect_error){
+        die("Erreur de connexion : " . $conn->connect_error);
+    }
+    
+} catch (Exception $e) {
+    die("Erreur : " . $e->getMessage());
+}
+
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
+}
+
+function requireLogin() {
+    if (!isLoggedIn()) {
+        header("Location: auth/login.php");
+        exit();
+    }
+}
+?>
